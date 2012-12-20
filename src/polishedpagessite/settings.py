2f3c431e32e81c7dbebe287aa5cd9287654a28ab
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2012-12-20 15:52:32 -0600

    Created local_settings.
    Added local_settings.py.template as well.

--- src/polishedpagessite/settings.py
+++ src/polishedpagessite/settings.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ImproperlyConfigured
+
 # Django settings for polishedpagessite project.
 
 DEBUG = True
@@ -9,16 +11,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+try:
+    from .local_settings import DATABASES, MONGO_DATABASE_NAME, LOGGING
+except ImportError as ex:
+    raise ImproperlyConfigured('Could not import local_settings. '
+        'Make sure you have created a local_settings file. ({})'.format(ex))
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -117,32 +114,3 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'polishedpages',
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
