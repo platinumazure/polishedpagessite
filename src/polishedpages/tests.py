26d667dff5690f72fcfe1d8c1f883a332e6fd60e
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2013-10-26 10:59:52 -0500

    Adding unit tests for registration form.

--- src/polishedpages/tests.py
+++ src/polishedpages/tests.py
@@ -1,16 +1,50 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+"""Unit tests for the polishedpages app."""
 from django.test import TestCase
+from django.contrib.auth.hashers import check_password
+from .forms import RegistrationForm
 
+class RegistrationFormTest(TestCase):
+    def test_form_invalid_if_password2_not_equal_to_password1(self):
+        """Form should not validate if password1 != password2."""
+        # arrange
+        target = RegistrationForm({
+            'email': 'test@example.com',
+            'password1': 'pw1',
+            'password2': 'invalid',
+        })
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        # act
+        result = target.is_valid()
+
+        # assert
+        self.assertFalse(result)
+
+    def test_form_valid_if_password2_equals_password1(self):
+        """Form should validate if password1 == password2, no other errors."""
+        # arrange
+        target = RegistrationForm({
+            'email': 'test@example.com',
+            'password1': 'passwd',
+            'password2': 'passwd',
+        })
+
+        # act
+        result = target.is_valid()
+
+        # assert
+        self.assertTrue(result)
+
+    def test_form_saves_user_with_correct_password(self):
+        """Form should create a new user with the correct password."""
+        # arrange
+        target = RegistrationForm({
+            'email': 'test@example.com',
+            'password1': 'passwd',
+            'password2': 'passwd',
+        })
+
+        # act
+        user = target.save()
+
+        # assert
+        self.assertTrue(check_password('passwd', user.password))
