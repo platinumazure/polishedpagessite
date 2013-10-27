2c3b0706644d66d408167462b13142f3556ff639
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2013-10-27 15:02:53 -0500

    Added TEMPLATE_DIRS to local settings.

--- src/polishedpagessite/settings.py
+++ src/polishedpagessite/settings.py
@@ -7,7 +7,8 @@ try:
     from .local_settings import (DATABASES, LOGGING, DEBUG, TEMPLATE_DEBUG,
         ADMINS, MANAGERS, TIME_ZONE, LANGUAGE_CODE, SITE_ID,
         USE_I18N, USE_L10N, USE_TZ,
-        MEDIA_ROOT, MEDIA_URL, STATIC_ROOT, STATICFILES_DIRS)
+        MEDIA_ROOT, MEDIA_URL, STATIC_ROOT, STATICFILES_DIRS,
+        TEMPLATE_DIRS)
 except ImportError as ex:
     raise ImproperlyConfigured('Could not import local_settings. '
         'Make sure you have created a local_settings file. ({})'.format(ex))
